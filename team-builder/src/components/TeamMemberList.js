7f011efcd70addb15624014344da9e417a4c99f8
lilyhoratio <lilyhzhou@gmail.com>
2019-07-24 14:58:21 -0700

    styled app page

--- team-builder/src/components/TeamMemberList.js
+++ team-builder/src/components/TeamMemberList.js
@@ -6,14 +6,17 @@ function TeamMemberList(props){
     console.log("props", memberList)
     
     return (
-        <div className="members-container">
-            {memberList.map(member => (
-                <div className="members-card">
-                    <h2>{member.name}</h2>
-                    <p>{member.email}</p>
-                    <p>{member.role}</p>
-                </div>
-            ))}
+        <div> 
+            <h2>Team Members!</h2>
+            <div className="members-container">
+                {memberList.map(member => (
+                    <div className="members-card">
+                        <h3>{member.name}</h3>
+                        <p>{member.email}</p>
+                        <p>{member.role}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
